8f5cfd8b4d339cca22d70265191e81aa58fb1452
laxmi singh <laxmi.singh@ge.com>
2019-03-05 21:01:38 +0530

    fhfhg

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,16 +1,32 @@
 import { Component } from '@angular/core';
+import { checkNoChanges } from '@angular/core/src/render3/instructions';
 
 @Component({
-    selector:'my-employee',
+    selector: 'my-employee',
     templateUrl: 'employee.component.html'
 })
 
 export class EmployeeComponent {
-    firstName : string = 'Laxmi';
-    lastName: string = 'Singh';
-    age : number = 28;
-    gender : String = 'female'
-    constructor(){
+    employeeList: any[] =
+        [{ Name: "laxmi", age: "28", height: '160cm' },
+        { Name: "Ashish", age: "30", height: "170cm" },
+        { Name: "Anmol", age: "22", height: "165cm" },
+        { Name: "Kamta", age: "58", height: "168cm" }]
+
+    checkAge(name : string) {
+       if(name === 'laxmi'){
+           console.log("28")
+          return "28"
+       }
+    } 
+    
+    fullname : string = "Laxmi";
+
+    constructor() {
         /* alert("employee component"); */
+    }
+
+    onSubmit(form){
+     alert("my form value  "+form);
     }
 }
