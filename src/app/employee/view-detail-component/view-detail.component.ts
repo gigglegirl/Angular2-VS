47ff0fc8e53e0368ffb77c511084b9f3f76d3bf8
laxmi singh <laxmi.singh@ge.com>
2019-03-11 21:20:26 +0530

    laxmi commit

--- src/app/employee/view-detail-component/view-detail.component.ts
+++ src/app/employee/view-detail-component/view-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-detail',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-detail.component.less']
 })
 export class ViewDetailComponent implements OnInit {
-
-  constructor() { }
+  pan_no:string
+  constructor(private _routeParameter : ActivatedRoute) { }
 
   ngOnInit() {
+    this.pan_no = this._routeParameter.snapshot.params['pan_no'];
+    
   }
+
 
 }
